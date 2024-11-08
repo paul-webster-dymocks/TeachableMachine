f8892fc5fdb1fa37aedca81d6fbaf779e976daf7
Lisa <L.carpenter@outlook.com>
2024-11-08 16:05:39 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,42 +3,46 @@ import streamlit as st
 import numpy as np
 #from keras.models import load_model
 
+from keras.models import load_model  # TensorFlow is required for Keras to work
+from PIL import Image, ImageOps  # Install pillow instead of PIL
+import numpy as np
 
-if st.button("CLICK ME"):
-    st.balloons()
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
 
+# Load the model
+model = load_model("keras_Model.h5", compile=False)
 
-# # Import the class labels from labels.txt and assign to a list
-# classes = [' '.join(x.split(' ')[1:]).replace('\n','') for x in open('labels.txt', 'r').readlines()]
-# # Load the Model
-# model = load_model('keras_model.h5', compile = False)
+# Load the labels
+class_names = open("labels.txt", "r").readlines()
 
-# # Create the streamlit Title and camera_input
-# st.title(f'Is it {classes[0]} or {classes[1]}!?')
-# img_file_buffer = st.camera_input(f"Take a picture of {classes[0]} or {classes[1]}")
+# Create the array of the right shape to feed into the keras model
+# The 'length' or number of images you can put into the array is
+# determined by the first position in the shape tuple, in this case 1
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
+# Replace this with the path to your image
+image = Image.open("snowexample.png").convert("RGB")
 
-# # Trigger when a photo has been taken and the bugger is no longer None
-# if img_file_buffer is not None:
-#     # Get the image and process it as required by the model
-#     # We are reshaping and converting the image to match the input the model requires.
-#     bytes_data = img_file_buffer.getvalue()
-#     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-#     image = cv2.resize(cv2_img, (224, 224), interpolation=cv2.INTER_AREA)
-#     image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
-#     image = (image / 127.5) - 1
-#     probabilities = model.predict(image)
+# resizing the image to be at least 224x224 and then cropping from the center
+size = (224, 224)
+image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
 
-#     # We now have the probabilities of the image being for either class
-#     # Check if either probability is over 80%, if so print the message for that classes.
-#     if probabilities[0,0] > 0.8:
-#         prob = round(probabilities[0,0] * 100,2)
-#         st.write(f"I'm {prob}% sure that's {classes[0]}!")
-#     elif probabilities[0,1] > 0.8:
-#         prob = round(probabilities[0,1] * 100,2)
-#         st.write(f"I'm {prob}% sure that's {classes[1]}!")
-#     else:
-#         st.write("I'm not confident that I know what this is! ")
+# turn the image into a numpy array
+image_array = np.asarray(image)
 
-#     # End on balloons
-#     st.balloons()
+# Normalize the image
+normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+
+# Load the image into the array
+data[0] = normalized_image_array
+
+# Predicts the model
+prediction = model.predict(data)
+index = np.argmax(prediction)
+class_name = class_names[index]
+confidence_score = prediction[0][index]
+
+# Print prediction and confidence score
+print("Class:", class_name[2:], end="")
+print("Confidence Score:", confidence_score)
